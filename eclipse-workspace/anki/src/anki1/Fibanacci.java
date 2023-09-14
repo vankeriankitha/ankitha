f4eac617a9a74b793734c5072635f00267ec20a1
Ankitha <vankerikankitha@gmail.com>
2023-09-14 12:47:17 +0530

    commit

--- eclipse-workspace/anki/src/anki1/Fibanacci.java
+++ eclipse-workspace/anki/src/anki1/Fibanacci.java
@@ -7,12 +7,12 @@ public class Fibanacci {
 		int num2=1;
 		int sum=0;
 		
-		System.out.println(num1+" "+num2);
+		System.out.print(num1+" "+num2);
 		
 		for(int i=2;i<=10;i++)
 		{
 			sum=num1+num2;
-			System.out.println(sum);
+			System.out.print(" "+sum);
 			num1=num2;
 			num2=sum;
 		}
