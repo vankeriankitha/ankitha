eb31de0e9fac492add6fd0036957f58cce74dcc2
Ankitha <vankerikankitha@gmail.com>
2023-09-14 12:30:27 +0530

    commit

---
+++ eclipse-workspace/anki/src/anki1/Factorial.java
@@ -0,0 +1,16 @@
+package anki1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int num=10;
+		int fact=1;
+		
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+		}
+        System.out.println(fact);
+	}
+
+}
