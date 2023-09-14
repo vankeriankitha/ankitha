29e734c1bb8921dd71db95fb758051b9300b0ca0
Ankitha <vankerikankitha@gmail.com>
2023-09-14 11:56:34 +0530

    commit

---
+++ eclipse-workspace/anki/src/anki1/swapping.java
@@ -0,0 +1,17 @@
+package anki1;
+
+public class swapping {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int temp=0;
+		
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b); 
+	}
+
+}
